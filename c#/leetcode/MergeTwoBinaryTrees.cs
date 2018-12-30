9d40d3e2c2543e284855996bf7b7fdd7b581a060
yerkebulan <abzhapbarov.erkon@ya.ru>
2018-12-31 00:00:35 +0600

    617. Merge Two Binary Trees, iterative approach

--- c#/leetcode/MergeTwoBinaryTrees.cs
+++ c#/leetcode/MergeTwoBinaryTrees.cs
@@ -1,13 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
 
+
+using System.Collections.Generic;
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     public int val;
+*     public TreeNode left;
+*     public TreeNode right;
+*     public TreeNode(int x) { val = x; }
+* }
+*/
 namespace MergeTwoBinaryTrees
 {
     public class TreeNode
@@ -22,12 +24,70 @@ namespace MergeTwoBinaryTrees
     {
         public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
         {
+            if (t1 == null && t2 == null) return null;
+            else if (t1 != null) return t1;
+            else if (t2 != null) return t2;
+            
             TreeNode mergedNode = new TreeNode(0);
-            this.MergeNodes(t1, t2, mergedNode);
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(t1);
+            stack.Push(t2);
+            stack.Push(mergedNode);
+
+            while (stack.Count > 0)
+            {
+                TreeNode currNode = stack.Pop();
+                TreeNode rightTreeNode = stack.Pop();
+                TreeNode leftTreeNode = stack.Pop();
+
+                MergeTwoNodes(leftTreeNode, rightTreeNode, currNode);
+                if (currNode == null) continue;
+
+                if (leftTreeNode?.left != null || rightTreeNode?.left != null)
+                {
+                    currNode.left = new TreeNode(0);
+                    stack.Push(leftTreeNode?.left);
+                    stack.Push(rightTreeNode?.left);
+                    stack.Push(currNode.left);
+                }
+
+                if (leftTreeNode?.right != null || rightTreeNode?.right != null)
+                {
+                    currNode.right = new TreeNode(0);
+                    stack.Push(leftTreeNode?.right);
+                    stack.Push(rightTreeNode?.right);
+                    stack.Push(currNode.right);
+                }
+            }
 
             return mergedNode;
         }
 
+        public TreeNode MergeTwoNodes(TreeNode t1, TreeNode t2, TreeNode resNode)
+        {
+            if (t1 != null && t2 != null)
+            {
+                resNode.val = t1.val + t2.val;
+            }
+            else if (t1 != null)
+            {
+                resNode.val = t1.val;
+            }
+            else if (t2 != null)
+            {
+                resNode.val = t2.val;
+            }
+            else
+            {
+                resNode = null;
+            }
+
+            return resNode;
+        }
+
+        /**
+        Recursive way
+         */
         public void MergeNodes(TreeNode t1, TreeNode t2, TreeNode currNode)
         {
             if (t1 != null && t2 != null)
@@ -48,16 +108,15 @@ namespace MergeTwoBinaryTrees
             }
 
             if ((t1 != null && t1.left != null) || (t2 != null && t2.left != null))
-            {                
+            {
                 currNode.left = new TreeNode(0);
                 MergeNodes(t1?.left, t2?.left, currNode.left);
             }
             if ((t1 != null && t1.right != null) || (t2 != null && t2.right != null))
-            {               
+            {
                 currNode.right = new TreeNode(0);
                 MergeNodes(t1?.right, t2?.right, currNode.right);
             }
-
         }
 
         public TreeNode GetTest1()
