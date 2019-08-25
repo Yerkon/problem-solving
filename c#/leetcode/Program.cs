35b782a1c17266d7747e85da82c9444dea5404a7
Yerkon <abzhapbarov.erkon@ya.ru>
2019-08-25 13:45:58 +0500

    414. Third Maximum Number:    // Time: O(N), Space: O(1)

--- c#/leetcode/Program.cs
+++ c#/leetcode/Program.cs
@@ -8,30 +8,7 @@ namespace leetcode {
         static void Main(string[] args) {
             //Solution sol = new Solution();
             Solution sol = new Solution();
-            //  sol.FindDisappearedNumbers (new int[] { 10, 2, 5, 10, 9, 1, 1, 4, 3, 7 });
-            // 
-
-            // [1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3]
-            // 516
-
-            sol.Rotate(new int[] {
-                8,
-                2,
-                0,
-                4,
-                1,
-                4,
-                2,
-                1,
-                0,
-                6,
-                6,
-                2
-            }, 12);
-
-            int b = sol.gcd(15, 65);
-
-            int c;
+            sol.ThirdMax(new int[] { 1, -2147483648, 2 });
         }
 
     }
