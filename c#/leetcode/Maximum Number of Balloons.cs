3b37bfb94a5289beffc5703d5016474a9fe87027
Yerkon <abzhapbarov.erkon@ya.ru>
2020-03-07 19:35:00 +0500

    1189. Maximum Number of Balloons

---
+++ c#/leetcode/Maximum Number of Balloons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum_Number_of_Balloons {
+    public class Solution {
+        public int MaxNumberOfBalloons(string text) {
+            int count = int.MaxValue;
+            string word = "balloon";
+            var dic = new Dictionary<char, int>();
+            var textDic = new Dictionary<char, int>();
+
+            foreach (char letter in word) {
+                dic[letter] = dic.GetValueOrDefault(letter, 0) + 1;
+            }
+
+            foreach (char letter in text) {
+                textDic[letter] = textDic.GetValueOrDefault(letter, 0) + 1;
+            }
+
+            foreach (char key in dic.Keys) {
+                int sourceCount = dic[key];
+                int textCount = textDic.GetValueOrDefault(key, 0);
+
+                count = Math.Min(count, textCount / sourceCount);
+            }
+
+            return count;
+        }
+    }
+}
