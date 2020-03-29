d4a3bb268ed0b522fd2d427a6b9459c9fe0ca3aa
Yerkon <abzhapbarov.erkon@ya.ru>
2020-03-29 21:22:43 +0500

    Flood Fill: no additional space

--- c#/leetcode/Flood Fill.cs
+++ c#/leetcode/Flood Fill.cs
@@ -9,41 +9,32 @@ namespace leetcode {
         public int[][] FloodFill(int[][] image, int sr, int sc, int newColor) {
             if (image.Length == 0) return image;
 
-            int[][] seen = new int[image.Length][];
+            int oldColor = image[sr][sc];
 
-            for (int i = 0; i < image.Length; i++) {
-                int[] row = new int[image[0].Length];
-                seen[i] = row;
+            if(oldColor != newColor) {
+                Fill(image, sr, sc, newColor, oldColor);
             }
-
-            Fill(seen, image, sr, sc, newColor, image[sr][sc]);
 
             return image;
         }
 
-        public void Fill(int[][] seen, int[][] image, int sr, int sc, int newColor, int oldColor) {
+        public void Fill(int[][] image, int sr, int sc, int newColor, int oldColor) {
             if (
                 sr < 0 || sr >= image.Length
                 || sc < 0 || sc >= image[0].Length) {
                 return;
             }
 
-            if (seen[sr][sc] == 1) {
-                return;
-            }
-
             if (image[sr][sc] == oldColor) {
                 image[sr][sc] = newColor;
-                seen[sr][sc] = 1;
-            } else {
-                return;
-            }
 
-            Fill(seen, image, sr + 1, sc, newColor, oldColor);
-            Fill(seen, image, sr - 1, sc, newColor, oldColor);
 
-            Fill(seen, image, sr, sc + 1, newColor, oldColor);
-            Fill(seen, image, sr, sc - 1, newColor, oldColor);
+                Fill(image, sr + 1, sc, newColor, oldColor);
+                Fill(image, sr - 1, sc, newColor, oldColor);
+
+                Fill(image, sr, sc + 1, newColor, oldColor);
+                Fill(image, sr, sc - 1, newColor, oldColor);
+            } 
         }
     }
 }
