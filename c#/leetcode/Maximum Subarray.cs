e8f400fdbf8e907f868ce0d299df5fa7cd6ac19f
Yerkon <abzhapbarov.erkon@ya.ru>
2020-04-04 00:11:41 +0500

    Maximum Subarray

---
+++ c#/leetcode/Maximum Subarray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode {
+    class Maximum_Subarray {
+
+        // Brute force
+        public int MaxSubArray1(int[] nums) {
+            int maxSubRange = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++) {
+                int sum = nums[i];
+                for (int j = i + 1; j < nums.Length; j++) {
+
+                    sum += nums[j];
+
+                    maxSubRange = Math.Max(maxSubRange, sum);
+                }
+            }
+
+            return maxSubRange;
+        }
+
+       
+    }
+}
