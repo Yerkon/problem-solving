18430a1105cfdbd23e84fcf608c2b1ca767bc783
Yerkon <abzhapbarov.erkon@ya.ru>
2020-04-07 00:53:08 +0500

    Group Anagrams

---
+++ c#/leetcode/Graph/Find the Town Judge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode.Graph {
+    class Find_the_Town_Judge {
+        public int FindJudge(int N, int[][] trust) {
+
+        }
+    }
+}
---
+++ c#/leetcode/Group Anagrams.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode {
+    internal class Group_Anagrams {
+        public IList<IList<string>> GroupAnagrams(string[] strs) {
+            var listOfList = new List<IList<string>>();
+            var strList = new List<string>();
+
+            foreach (string str in strs) {
+                string orderedStr = string.Join("",str.OrderBy(r => r));                
+                int index = strList.IndexOf(orderedStr);
+                
+                if(index > -1) {
+                    listOfList[index].Add(str);
+                } else {
+                    strList.Add(orderedStr);
+                    var list = new List<string>() { str };
+                    listOfList.Add(list);
+                }
+            }
+
+            return listOfList;
+        }
+
+        public IList<IList<string>> GroupAnagrams1(string[] strs) {
+            var listOfList = new List<IList<string>>();
+            var listOfDic = new List<Dictionary<char, int>>();
+
+            foreach (string str in strs) {
+                var wordDic = new Dictionary<char, int>();
+
+                if (str.Length == 0) {
+                    char empty = ' ';
+                    wordDic[empty] = wordDic.GetValueOrDefault(empty, 0) + 1;
+                }
+
+                foreach (char letter in str) {
+                    wordDic[letter] = wordDic.GetValueOrDefault(letter, 0) + 1;
+                }
+
+                // check in dictionary
+                bool hasInDict = false;
+                int i = 0;
+
+                for (i = 0; i < listOfDic.Count; i++) {
+                    Dictionary<char, int> dic = listOfDic[i];
+                    bool isFound = true;
+
+                    foreach (char key in wordDic.Keys) {
+                        if (wordDic.Count != dic.Count || !dic.ContainsKey(key) || wordDic[key] != dic[key]) {
+                            isFound = false;
+                            break;
+                        }
+                    }
+
+                    if (isFound) {
+                        hasInDict = true;
+                        break;
+                    }
+                }
+
+                if (!hasInDict) {
+                    listOfDic.Add(wordDic);
+                    var groupStrs = new List<string>() { str };
+                    listOfList.Add(groupStrs);
+                } else {
+                    listOfList[i].Add(str);
+                }
+            }
+
+            return listOfList;
+        }
+
+    }
+}
