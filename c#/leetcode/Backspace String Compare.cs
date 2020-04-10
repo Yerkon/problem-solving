3cf7c466929b5857eab3cf6ab7e59598b75b3019
Yerkon <abzhapbarov.erkon@ya.ru>
2020-04-10 11:02:36 +0500

    Backspace String Compare

---
+++ c#/leetcode/Backspace String Compare.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode {
+    class Backspace_String_Compare {
+            public bool BackspaceCompare(string S, string T) {
+
+                int i = S.Length - 1, j = T.Length - 1;
+                int iGridCount = 0, jGridCount = 0;
+
+                while (i >= 0 && j >= 0) {
+                    if (S[i] == '#') {
+                        iGridCount++;
+                        i--;
+                        continue;
+                    }
+
+                    if (T[j] == '#') {
+                        jGridCount++;
+                        j--;
+                        continue;
+                    }
+
+                    // clear # first
+                    if (iGridCount > 0) {
+                        i--;
+                        iGridCount--;
+                        continue;
+                    }
+
+                    if (jGridCount > 0) {
+                        j--;
+                        jGridCount--;
+                        continue;
+                    }
+
+                    if (S[i] == T[j]) {
+                        i--;
+                        j--;
+                    } else {
+                        return false;
+                    }
+                }
+
+
+                while (i >= 0 && (S[i] == '#' || iGridCount > 0)) {
+
+                    if (S[i] == '#') {
+                        i--;
+                        iGridCount++;
+                    } else if (iGridCount > 0) {
+                        i--;
+                        iGridCount--;
+                    }
+                }
+
+                // 
+
+                while (j >= 0 && (T[j] == '#' || jGridCount > 0)) {
+
+                    if (T[j] == '#') {
+                        j--;
+                        jGridCount++;
+                    } else if (jGridCount > 0) {
+                        j--;
+                        jGridCount--;
+                    }
+                }
+
+                return i == j;
+            }
+    }
+}
---
+++ c#/leetcode/Middle of the Linked List.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode {
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    internal class Middle_of_the_Linked_List {
+        public ListNode MiddleNode(ListNode head) {
+            if (head == null) return null;
+
+            int count = 0;
+
+            ListNode currNode = head;
+
+            while (currNode != null) {
+                count++;
+                currNode = currNode.next;
+            }
+
+            int middle = count / 2 + 1;
+            count = 1;
+
+            currNode = head;
+
+            while (count != middle) {
+                currNode = currNode.next;
+                count++;
+            }
+
+            return currNode;
+        }
+    }
+}
