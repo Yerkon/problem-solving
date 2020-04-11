e82aee512272ccb9b6c4686f6c64bf4bd096f471
Yerkon <abzhapbarov.erkon@ya.ru>
2020-04-11 22:41:19 +0500

    min stask with linked list

--- c#/leetcode/Min Stack.cs
+++ c#/leetcode/Min Stack.cs
@@ -6,16 +6,71 @@ using System.Text;
 namespace leetcode {
 
     class Min_Stack {
+
+        class Node {
+            public int Value { get; set; }
+            public int Min { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int val, int min) {
+                this.Value = val;
+                this.Min = min;
+            }
+
+            public Node(int val, int min, Node next) {
+                this.Value = val;
+                this.Min = min;
+                this.Next = next;
+            }
+        }
+
+        // Linked list
         public class MinStack {
 
-            int[] arr = new int[1];
-            SortedList<int, int> sortedList = new SortedList<int, int>();
-            int it = -1;
+            Node head;
             /** initialize your data structure here. */
             public MinStack() {
 
             }
 
+            // O(1)
+            public void Push(int x) {
+                if(head == null) {
+                    head = new Node(x, x);
+                } else {
+                    head = new Node(x, Math.Min(head.Min, x), head);
+                }
+            }
+
+            // O(1)
+            public void Pop() {
+                head = head.Next;
+            }
+
+            // O(1)
+            public int Top() {
+                return head.Value;
+            }
+
+            // O(1)
+            public int GetMin() {
+                return head.Min;
+            }
+        }
+
+
+        // Sorted list, array
+        public class MinStack1 {
+
+            int[] arr = new int[1];
+            SortedList<int, int> sortedList = new SortedList<int, int>();
+            int it = -1;
+            /** initialize your data structure here. */
+            public MinStack1() {
+
+            }
+
+            // O(n)
             public void Push(int x) {
 
                 if(it + 1 < arr.Length) {
@@ -34,12 +89,12 @@ namespace leetcode {
 
                     arr[++it] = x;
                 }
-                
+
                 sortedList[x] = sortedList.GetValueOrDefault(x, 0) + 1;
 
             }
 
-            // O(n)	
+            // O(logn) in average case, O(N) when remove from sorted list	
             public void Pop() {
                 if (it < 0) return;
 
@@ -61,7 +116,7 @@ namespace leetcode {
 
             // O(1)
             public int GetMin() {
-                return sortedList.FirstOrDefault().Key;
+                return sortedList.Keys[0];
             }
         }
 
