98584cccc6c2dedbcac8c46b1660a4eda0c0766b
Yerkon <abzhapbarov.erkon@ya.ru>
2020-04-24 22:51:47 +0500

    Bitwise AND of Numbers Range

---
+++ c#/leetcode/Bitwise AND of Numbers Range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode {
+    class Bitwise_AND_of_Numbers_Range {
+        public int RangeBitwiseAnd(int m, int n) {
+            int val = 0;
+
+            for (int i = 31; i >= 0; i--) {
+                int k = m & (1 << i);
+                int t = n & (1 << i);
+
+                if (k > 0 && k == t) {
+                    val += (int)Math.Pow(2, i);
+
+                } else if (k > 0 || t > 0) {
+                    return val;
+                }
+            }
+
+            return val;
+        }
+    }
+}
