d4bbe0968130d4a737c0d523118b0fcf45028bd0
Yerkon <abzhapbarov.erkon@ya.ru>
2020-09-06 23:16:49 +0600

    1277. Count Square Submatrices with All Ones: bottom-up. in progress

--- c#/leetcode/DP/CountSquares.cs
+++ c#/leetcode/DP/CountSquares.cs
@@ -5,14 +5,60 @@ using System.Text;
 namespace Project.DP {
     public class Solution {
 
-        // Recursive way
+        // bottom- up. in progress
         public int CountSquares(int[][] matrix) {
+            int count = 0;
+            int[][] dp = new int[matrix.Length][];
+
+            for (int i = 0; i < matrix.Length; i++) {
+                int[] dpRow = new int[matrix[i].Length];
+
+                dp[i] = dpRow;
+            }
+
+
+            for (int i = 0; i < matrix.Length; i++) {
+                for (int j = 0; j < matrix[i].Length; j++) {
+
+                    int prevTop = i - 1 < 0 ? 0 : dp[i - 1][j];
+                    int prevLeft = j - 1 < 0 ? 0 : dp[i][j - 1];
+                    int prevTopLeft = 0;
+
+                    if (i - 1 < 0 && j - 1 < 0) {
+                        prevTopLeft = 0;
+                    } else if (i - 1 < 0) {
+                        prevTopLeft = prevLeft;
+
+                    } else if (j - 1 < 0) {
+                        prevTopLeft = prevTop;
+                    }
+
+                    dp[i][j] = matrix[i][j] + Math.Min(Math.Min(prevTop, prevLeft), prevTopLeft);
+                    count += dp[i][j];
+                }
+            }
+
+            for (int i = 0; i < dp.Length; i++) {
+                for (int j = 0; j < dp[i].Length; j++) {
+                    Console.WriteLine(dp[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            return count;
+        }
+
+
+
+        // Recursive way. Top-down
+        public int CountSquares1(int[][] matrix) {
             int count = 0;
             int[][] memo = new int[matrix.Length][];
 
             for (int i = 0; i < matrix.Length; i++) {
                 int[] memoRow = new int[matrix[i].Length];
-                
+
                 for (int j = 0; j < memoRow.Length; j++) {
                     memoRow[j] = -1;
                 }
@@ -22,9 +68,9 @@ namespace Project.DP {
 
 
             for (int i = 0; i < matrix.Length; i++) {
-               
+
                 for (int j = 0; j < matrix[i].Length; j++) {
-                   if(matrix[i][j] == 1) {
+                    if (matrix[i][j] == 1) {
                         count += CountSquares(matrix, i, j, memo);
                     }
                 }
@@ -34,11 +80,11 @@ namespace Project.DP {
         }
 
         public int CountSquares(int[][] matrix, int i, int j, int[][] memo) {
-            if(i >= matrix.Length || j >= matrix[i].Length || matrix[i][j] == 0) {
+            if (i >= matrix.Length || j >= matrix[i].Length || matrix[i][j] == 0) {
                 return 0;
             }
 
-            if(memo[i][j] != -1) {
+            if (memo[i][j] != -1) {
                 return memo[i][j];
             }
 
