fd032299760501e88af3b106a580748097a5b054
Yerkon <abzhapbarov.erkon@ya.ru>
2020-10-11 00:56:22 +0600

    807. Max Increase to Keep City Skyline

---
+++ c#/leetcode/10-10-2020/KidsWithCandies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project._10_10_2020 {
+    public class Solution {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+            var res = new List<bool>();
+
+            int maxCandy = candies.Max();
+
+            for (int i = 0; i < candies.Length; i++) {
+                int currAmount = candies[i] + extraCandies;
+
+                bool hasGreater = currAmount >= maxCandy;
+                res.Add(hasGreater);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ c#/leetcode/10-10-2020/MaxIncreaseKeepingSkyline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._10_10_2020 {
+    public class Solution_MaxIncreaseKeepingSkyline {
+        public int MaxIncreaseKeepingSkyline(int[][] grid) {
+            int sum = 0;
+
+            int[] topSkyLine = new int[grid[0].Length];
+            int[] leftSkyLine = new int[grid.Length];
+
+            for (int i = 0; i < grid.Length; i++) {
+                for (int j = 0; j < grid[i].Length; j++) {
+                    leftSkyLine[i] = Math.Max(leftSkyLine[i], grid[i][j]);
+                    topSkyLine[i] = Math.Max(topSkyLine[i], grid[j][i]);
+                }
+            }
+
+            for (int i = 0; i < grid.Length; i++) {
+                for (int j = 0; j < grid[i].Length; j++) {
+                    int addLeftHeight = Math.Max(0, leftSkyLine[i] - grid[i][j]);
+                    int addTopHeight = Math.Max(0, topSkyLine[j] - grid[i][j]);
+
+                    int addSum = Math.Min(addLeftHeight, addTopHeight);
+                    grid[i][j] += addSum;
+
+                    sum += addSum;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
