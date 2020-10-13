36b24f98cd6300d8766403a83c8f83afe23e662e
Yerkon <abzhapbarov.erkon@ya.ru>
2020-10-13 23:12:12 +0600

    1470. Shuffle the Array

---
+++ c#/leetcode/13-10-2020/Shuffle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._13_10_2020 {
+    public class Solution_Shuffle {
+        public int[] Shuffle(int[] nums, int n) {
+            int[] res = new int[nums.Length];
+            int xPos = 0, yPos = n;
+
+            for (int i = 0; i < nums.Length; i++) {
+                if (i % 2 == 0) {
+                    res[i] = nums[xPos++];
+                } else {
+                    res[i] = nums[yPos++];
+                }
+            }
+
+            return res;
+        }
+    }
+}
