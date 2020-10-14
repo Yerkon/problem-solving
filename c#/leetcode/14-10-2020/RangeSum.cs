f7948d69b930d1bb2d8a45fe8fe0ed15cfb186d2
Yerkon <abzhapbarov.erkon@ya.ru>
2020-10-14 22:32:56 +0600

    1508. Range Sum of Sorted Subarray Sums

---
+++ c#/leetcode/14-10-2020/RangeSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._14_10_2020 {
+    public class RangeSum_Solution {
+        public int RangeSum(int[] nums, int n, int left, int right) {
+            
+            int sum = 0;
+            int mod = 1000000007;
+            int[] res = new int[n * (n + 1) / 2];
+            int pos = 0;
+
+            for (int i = 0; i < nums.Length; i++) {
+                int currSum = nums[i];
+
+                for (int j = i + 1; j < nums.Length; j++) {
+                    res[pos++] = currSum;
+                    currSum = currSum % mod + nums[j] % mod;
+                }
+
+                res[pos++] = currSum;
+            }
+
+            Array.Sort(res);
+
+            for (int i = left - 1; i < right; i++) {
+                sum = sum % mod + res[i] % mod;
+            }
+
+            return sum;
+        }
+    }
+}
