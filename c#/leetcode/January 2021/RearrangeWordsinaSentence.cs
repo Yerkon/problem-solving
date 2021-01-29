b6ff33b703db077a1ac5c5d98716fce8a843ca7e
Yerkon <abzhapbarov.erkon@ya.ru>
2021-01-29 19:34:12 +0600

    1451. Rearrange Words in a Sentence

---
+++ c#/leetcode/January 2021/RearrangeWordsinaSentence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode_2021 {
+    public class Solution {
+        public string ArrangeWords1(string text) {
+            string[] words = text.ToLower().Split(" ");
+            words = words.OrderBy(r => r.Length).ToArray();
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < words.Length; i++) {
+                string word = words[i];
+
+                if (i == 0) word = char.ToUpper(word[0]) + word.Substring(1);
+              
+                builder.Append(word);
+                builder.Append(" ");
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+
+            return builder.ToString();
+        }
+
+        public string ArrangeWords(string text) {
+            string[] words = text.ToLower().Split(" ");
+            words = words.OrderBy(r => r.Length).ToArray();
+
+            string res = string.Join(" ", words);
+
+            return char.ToUpper(res[0]) + res.Substring(1);
+        }
+    }
+}
--- c#/leetcode/Program.cs
+++ c#/leetcode/Program.cs
@@ -1,18 +1,16 @@
-﻿using BinaryTree2;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace leetcode {
-    internal class Program {
+namespace leetcode_2021 {
+    public class Program {
         private static void Main(string[] args) {
 
-            var sol = new Perm();
+            var sol = new Solution();
+            sol.ArrangeWords("Leetcode is cool");
 
-            sol.search();
 
-
-           
             //  sol.MinPathSum()
         }
 
