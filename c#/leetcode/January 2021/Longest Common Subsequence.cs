b5468fbd190b3a12d969b37f916e068e9b41d5ab
Yerkon <abzhapbarov.erkon@ya.ru>
2021-02-07 00:56:54 +0600

    1143. Longest Common Subsequence: in progress

---
+++ c#/leetcode/January 2021/Longest Common Subsequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.January_2021 {
+    internal class Longest_Common_Subsequence {
+        public int LongestCommonSubsequence(string text1, string text2) {
+            int longest = 0;
+
+            var set1 = new HashSet<string>();
+            var set2 = new HashSet<string>();
+
+            for (int i = 0; i < text1.Length; i++) {
+                CountSubs(text1, new StringBuilder(), i, set1);
+            }
+
+            for (int i = 0; i < text2.Length; i++) {
+                CountSubs(text2, new StringBuilder(), i, set2);
+            }
+
+            foreach (string item in set1) {
+                Console.WriteLine(item);
+                if(set2.Contains(item)) {
+                    longest = Math.Max(longest, item.Length);
+                }
+            }
+
+            return longest;
+        }
+
+        public void CountSubs(string text, StringBuilder currStr, int currIdx, HashSet<string> set) {
+
+            //string curr = currStr +  text1[currIdx];
+            var newBuilder = new StringBuilder(currStr.ToString());
+            newBuilder.Append(text[currIdx]);
+
+            for (int i = currIdx + 1; i < text.Length; i++) {
+                CountSubs(text, newBuilder, i, set);
+            }
+
+            set.Add(newBuilder.ToString());
+        }
+    }
+}
