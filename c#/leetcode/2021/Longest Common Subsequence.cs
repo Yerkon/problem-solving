d37d13941338ac1747ff2b37ae7c542fc5ef86f0
Yerkon <abzhapbarov.erkon@ya.ru>
2021-02-09 09:35:16 +0600

    1143. Longest Common Subsequence: memory optimize

--- c#/leetcode/2021/Longest Common Subsequence.cs
+++ c#/leetcode/2021/Longest Common Subsequence.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Project.January_2021 {
     internal class Longest_Common_Subsequence {
-        public int LongestCommonSubsequence(string text1, string text2) {
+        public int LongestCommonSubsequence1(string text1, string text2) {
             
             int[][] dp = new int[text1.Length + 1][];
 
@@ -24,6 +24,29 @@ namespace Project.January_2021 {
             }
 
             return dp[text1.Length][text2.Length];
+        }
+
+        // Time: O(MN), Space: O(M)
+        public int LongestCommonSubsequence(string text1, string text2) {
+
+            int[][] dp = new int[text1.Length + 1][];
+
+            for (int i = 0; i < 2; i++) {
+                int[] dpRow = new int[text2.Length + 1];
+
+                dp[i] = dpRow;
+            }
+
+            for (int i = 1; i <= text1.Length; i++) {
+                for (int j = 1; j <= text2.Length; j++) {
+                    int equal = text1[i - 1] == text2[j - 1] ? 1 : 0;
+                    int k = equal + dp[(i - 1)%2][j - 1];   
+
+                    dp[i % 2][j] = Math.Max(k, Math.Max(dp[i % 2][j - 1], dp[(i - 1)%2][j]));
+                }
+            }
+
+            return dp[text1.Length % 2][text2.Length];
         }
 
         public void CountSubs(string text, string currStr, int currIdx) {
