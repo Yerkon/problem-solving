62926f911cd84b6eb268a34f849e9d7081662b72
Yerkon <abzhapbarov.erkon@ya.ru>
2021-02-16 18:25:03 +0600

    669. Trim a Binary Search Tree: Recursive way

---
+++ c#/leetcode/2021/Trim a Binary Search Tree.cs
@@ -0,0 +1,35 @@
+﻿using BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+namespace Project._2021 {
+    public class Trim_a_Binary_Search_Tree {
+        public TreeNode TrimBST(TreeNode root, int L, int R) {
+            if (root is null) return null;
+
+            if (root.val < L) {
+                return TrimBST(root.right, L, R);
+            } else if(root.val > R) {
+                return TrimBST(root.left, L, R);
+            }
+
+            // in range
+            root.left = TrimBST(root.left, L, R);
+            root.right = TrimBST(root.right, L, R);
+
+            return root;
+        }
+
+    }
+}
