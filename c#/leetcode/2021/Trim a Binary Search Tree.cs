083c7fbffa1a08b6c6095321a9e241ae163c0be9
Yerkon <abzhapbarov.erkon@ya.ru>
2021-02-19 09:33:42 +0600

    669. Trim a Binary Search Tree: iterative in progress

--- c#/leetcode/2021/Trim a Binary Search Tree.cs
+++ c#/leetcode/2021/Trim a Binary Search Tree.cs
@@ -15,7 +15,7 @@ using System.Text;
 
 namespace Project._2021 {
     public class Trim_a_Binary_Search_Tree {
-        public TreeNode TrimBST(TreeNode root, int L, int R) {
+        public TreeNode TrimBST1(TreeNode root, int L, int R) {
             if (root is null) return null;
 
             if (root.val < L) {
@@ -29,6 +29,51 @@ namespace Project._2021 {
             root.right = TrimBST(root.right, L, R);
 
             return root;
+        }
+
+
+        // iterative
+        public TreeNode TrimBST(TreeNode root, int L, int R) {
+            if (root is null) return null;
+            
+            TreeNode newRoot = null;
+            TreeNode parent = null;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0) {
+                TreeNode curr = stack.Pop();
+
+                if(curr is null) {
+                    continue;
+                }
+
+                if(curr.val < L) {
+                    stack.Push(curr.right);
+
+                    if(parent != null) parent.left = curr.right;
+
+                    continue;
+                } else if(curr.val > R ) {
+                    stack.Push(curr.left);
+
+                    if(parent != null) parent.right = curr.left;
+
+                    continue;
+                }
+
+                if(newRoot is null) {
+                    newRoot = curr;
+                }
+
+                parent = curr;
+                stack.Push(curr.left);
+                stack.Push(curr.right);
+               
+            }
+
+            return newRoot;
         }
 
     }
