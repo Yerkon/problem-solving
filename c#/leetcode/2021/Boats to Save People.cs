b3b5c46ed371723a7f2156fb231b008888e258e0
Yerkon <abzhapbarov.erkon@ya.ru>
2021-03-10 09:30:20 +0600

    881. Boats to Save People

--- c#/leetcode/2021/Boats to Save People.cs
+++ c#/leetcode/2021/Boats to Save People.cs
@@ -5,11 +5,50 @@ using System.Text;
 namespace Project._2021 {
     public class Boats_to_Save_People {
 
+        public int NumRescueBoats(int[] people, int limit) {
+            int boats = 0;
+            int[] countArr = new int[limit + 1];
+
+            for (int i = 0; i < people.Length; i++) {
+                countArr[people[i]]++;
+            }
+
+            for (int weight = limit; weight >= 1; weight--) {
+                
+                while (countArr[weight] > 0) {
+                    int rem = limit - weight;
+                    countArr[weight]--;
+                    bool remFound = false;
+
+                    if (rem == 0) {
+                        boats++;
+                    } else {
+                        
+                        int startEl = Math.Min(rem, weight);
+                        for (int k = startEl; k >= 1; k--) {
+                            if(countArr[k] > 0) {
+                                countArr[k]--;
+                                boats++;
+                                remFound = true;
+                                break;
+                            }
+                        }
+
+                        if (!remFound) boats++;
+                       
+                    }
+                }
+            }
+
+            return boats;
+        }
+
+     
 
         // [3,2,2,1]
         // [0, 0, 2, 0]
         // 3
-        public int NumRescueBoats(int[] people, int limit) {
+        public int NumRescueBoats1(int[] people, int limit) {
             int boats = 0;
             int maxI = 0, maxJ = 1;
 
@@ -23,12 +62,12 @@ namespace Project._2021 {
                     continue;
                 }
 
-                int currMaxW = int.MinValue;
+                int ithMaxW = int.MinValue;
                 bool sumFound = false;
                 maxI = i; maxJ = -1;
 
                 for (int j = i + 1; j < people.Length; j++) {
-                    if (people[j] == 0 || sumFound) continue;
+                    if (people[j] == 0) continue;
 
                     int twoSum = people[i] + people[j];
                    
@@ -38,12 +77,12 @@ namespace Project._2021 {
                         people[j] = 0;
                         sumFound = true;
 
-                        continue;
+                        break;
 
                     } else {
                         // find min
-                        if(twoSum > currMaxW && twoSum <= limit) {
-                            currMaxW = twoSum;
+                        if(twoSum > ithMaxW && twoSum <= limit) {
+                            ithMaxW = twoSum;
                             maxI = i; maxJ = j;
                         }
                     }
@@ -51,7 +90,7 @@ namespace Project._2021 {
 
                 if (sumFound) continue;
 
-                // max i, j found
+                // max i, j? found
                 people[maxI] = 0;
                 if(maxJ != -1) people[maxJ] = 0;
                 boats++;
@@ -59,8 +98,6 @@ namespace Project._2021 {
 
             return boats;
         }
-
-
     }
 }
 
