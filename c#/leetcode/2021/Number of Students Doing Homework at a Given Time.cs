6eb0f93c9215a5e8cdcf7dd6a6e98be47295639b
Yerkon <abzhapbarov.erkon@ya.ru>
2021-03-30 08:48:00 +0600

    Number_of_Students_Doing_Homework_at_a_Given_Time

---
+++ c#/leetcode/2021/Number of Students Doing Homework at a Given Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._2021 {
+    internal class Number_of_Students_Doing_Homework_at_a_Given_Time {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime) {
+            int count = 0;
+
+            for (int i = 0; i < startTime.Length; i++) {
+              
+                if (startTime[i] <= queryTime && queryTime <= endTime[i]) count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ c#/leetcode/2021/Print FooBar Alternately.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._2021 {
+    class Print_FooBar_Alternately {
+        public class FooBar {
+            private int n;
+
+            public FooBar(int n) {
+                this.n = n;
+            }
+
+            public void Foo(Action printFoo) {
+
+                for (int i = 0; i < n; i++) {
+
+                    // printFoo() outputs "foo". Do not change or remove this line.
+                    printFoo();
+                }
+            }
+
+            public void Bar(Action printBar) {
+
+                for (int i = 0; i < n; i++) {
+
+                    // printBar() outputs "bar". Do not change or remove this line.
+                    printBar();
+                }
+            }
+        }
+    }
+}
