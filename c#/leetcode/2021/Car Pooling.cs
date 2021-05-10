42ef12735b1050224232ea6e1152d9e2b7271af5
Yerkon <abzhapbarov.erkon@ya.ru>
2021-05-10 19:58:14 +0600

    1094. Car Pooling

---
+++ c#/leetcode/2021/Car Pooling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// https://leetcode.com/problems/car-pooling/
+
+
+namespace Project._2021 {
+    public class Car_Pooling {
+        public bool CarPooling(int[][] trips, int capacity) {
+            int[] road = new int[1001];
+            
+            for (int i = 0; i < trips.Length; i++) {
+
+                int[] trip = trips[i];
+              
+                for (int k = trip[1]; k <= trip[2]; k++) {
+                    road[k] += trip[0];
+                }
+            }
+
+            for (int i = 0; i < trips.Length; i++) {
+
+                int[] trip = trips[i];
+                road[trip[2]] -= trip[0];
+            }
+
+
+            for (int i = 0; i < road.Length; i++) {
+                if (road[i] > capacity) return false;
+            }
+
+            return true;
+        }
+    }
+}
