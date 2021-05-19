2bc889d592c42290ab095a612c98f2297d0cb1c1
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-05-19 09:17:36 +0600

    [310] Minimum Height Trees

--- c#/leetcode/2021/310.minimum-height-trees.cs
+++ c#/leetcode/2021/310.minimum-height-trees.cs
@@ -14,14 +14,13 @@ public class Solution {
         var result = new List<int>();
         var adjList = new List<List<int>>();
         var distance = new List<int[]>();
-        
+
         for (int i = 0; i < n; i++) {
             adjList.Add(new List<int>());
 
             int[] items = new int[n];
-            for (int k = 0; k < n; k++)
-            {
-                items[k] = -1;           
+            for (int k = 0; k < n; k++) {
+                items[k] = -1;
             }
             distance.Add(items);
         }
@@ -35,25 +34,35 @@ public class Solution {
 
         int[] heights = new int[n];
         int minHeight = int.MaxValue;
-      
-        for (int i = 0; i < n; i++) {
-           heights[i] = GetHeightForNode(i, adjList, n, distance); 
-           minHeight = Math.Min(minHeight, heights[i]);
+
+        for (int i = 0; i < distance.Count; i++) {
+            for (int j = 0; j < distance[i].Length; j++) {
+
+                if(distance[i][j] == -1) {
+                     FillDistanceForNode(j, adjList, n, distance);
+                }
+
+                heights[i] = Math.Max(heights[i], distance[i][j]);
+            }
+
+             minHeight = Math.Min(minHeight, heights[i]);
         }
 
-        // for (int i = 0; i < distance.Count; i++)
+        // for (int i = 0; i < heights.Length; i++)
         // {
+        //     Console.WriteLine($"Height {i}: {heights[i]}");
+        // }
+       
+        // for (int i = 0; i < distance.Count; i++) {
         //     Console.WriteLine($"Distance from {i} to ");
-        //     for (int j = 0; j < distance[i].Length; j++)
-        //     {
+        //     for (int j = 0; j < distance[i].Length; j++) {
         //         Console.WriteLine($"{j} = {distance[i][j]}");
         //     }
         // }
 
-        for (int i = 0; i < heights.Length; i++)
-        {
-           // Console.WriteLine($"node: {i}: " + heights[i]);
-            if(heights[i] == minHeight) {
+        for (int i = 0; i < heights.Length; i++) {
+            // Console.WriteLine($"node: {i}: " + heights[i]);
+            if (heights[i] == minHeight) {
                 result.Add(i);
             }
         }
@@ -62,48 +71,33 @@ public class Solution {
     }
 
 
-      public int GetHeightForNode(int node, List<List<int>> adjList, int n, List<int[]> gDistance) {
+    public void FillDistanceForNode(int node, List<List<int>> adjList, int n, List<int[]> gDistance) {
         bool[] visited = new bool[n];
-        int maxDistance = 0;
         Queue<int> q = new Queue<int>();
-
         visited[node] = true;
         gDistance[node][node] = 0;
         q.Enqueue(node);
-        
-        while (q.Count > 0) {
-            int currNode = q.Dequeue();
 
+        while (q.Count > 0) {
+            int currNode = q.Dequeue();// 0, 1, 2, 3
+            gDistance[currNode][currNode] = 0;
+            
             // iterate over direct connections
-            foreach (int u in adjList[currNode]) {
+            foreach (int u in adjList[currNode]) { // 1, [2, 3]
                 if (visited[u]) continue;
 
-                if(gDistance[node][u] != -1) {
-                    visited[u] = true;
-                    maxDistance = Math.Max(maxDistance, gDistance[node][u]);
-                    q.Enqueue(u);
-                    continue;
-                } else if(gDistance[u][node] != -1) {
-                    visited[u] = true;
-                     maxDistance = Math.Max(maxDistance, gDistance[u][node]);
-                    q.Enqueue(u);
-                    continue;
-                }
-                
-                visited[u] = true;
-             
-                gDistance[node][u] = gDistance[node][currNode] + 1;
-                gDistance[u][node] = gDistance[node][currNode] + 1;
+                visited[u] = true; // 0, 1, 2, 3
+                gDistance[node][u] = gDistance[node][currNode] + 1; // (0-1=1), (0-2=2), (0-3=2)
+                gDistance[u][node] = gDistance[node][currNode] + 1; // (1-0=1), (2-0=2), (3-0=2)
+                gDistance[currNode][u] = 1;
+                gDistance[u][currNode] = 1;
 
-                maxDistance = Math.Max(maxDistance, gDistance[node][u]);
-                q.Enqueue(u);
+                q.Enqueue(u); // 1, 2, 3
             }
         }
-
-        return maxDistance;
     }
 
- public int[] GetDistanceForNode(int node, List<List<int>> adjList, int n) {
+    public int[] GetDistanceForNode(int node, List<List<int>> adjList, int n) {
         bool[] visited = new bool[n];
         int[] distance = new int[n];
         Queue<int> q = new Queue<int>();
@@ -111,7 +105,7 @@ public class Solution {
         visited[node] = true;
         distance[node] = 0;
         q.Enqueue(node);
-        
+
 
         while (q.Count > 0) {
             int currNode = q.Dequeue();
@@ -127,15 +121,14 @@ public class Solution {
             }
         }
 
-         for (int i = 0; i < distance.Length; i++)
-        {
+        for (int i = 0; i < distance.Length; i++) {
             Console.WriteLine($"Distance from {node} to {i} : {distance[i]}");
-        } 
+        }
 
         return distance;
     }
 
-  
+
 }
 // @lc code=end
 
