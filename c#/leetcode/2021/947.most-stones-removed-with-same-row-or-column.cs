9265dcdd3196698e155cbea8819fdca6aa9b35fd
Yerkon <abzhapbarov.erkon@ya.ru>
2021-05-30 11:26:50 +0600

    [947] Most Stones Removed with Same Row or Column

--- c#/leetcode/2021/947.most-stones-removed-with-same-row-or-column.cs
+++ c#/leetcode/2021/947.most-stones-removed-with-same-row-or-column.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 /*
  * @lc app=leetcode id=947 lang=csharp
@@ -11,42 +11,109 @@ using System.Text;
 
 // @lc code=start
 public class Solution {
+
+    string[, ] plane = new string[10000, 10000];
+    int[, ] size = new int[10000, 10000];
+
     public int RemoveStones(int[][] stones) {
         int res = 0;
-        bool[,] visited = new bool[10000, 10000];
-        int[,] plane = new int[10000, 10000];
+        bool[, ] visited = new bool[10000, 10000];
+        int[, ] matrix = new int[10000, 10000];
 
-        int size = 0;
+        int maxLength = 0;
+
         for (int i = 0; i < stones.Length; i++) {
             int r = stones[i][0];
             int c = stones[i][1];
 
-            plane[r, c] = 1;
+            plane[r, c] = r + "#" + c;
+            size[r, c] = 1;
 
-            size = Math.Max(c, Math.Max(size, r));
+            maxLength = Math.Max(c, Math.Max(maxLength, r));
         }
 
-        for (int i = 0; i <= size; i++) {
-            for (int j = 0; j <= size; j++) {
-                // Console.WriteLine($"iterate {i} {j}");
-
-                if (!visited[i, j] && plane[i, j] == 1) {
-
-                    // Console.WriteLine($"i: {i} j: {j}");
-                    CountRemoveStones(plane, visited, i, j, ref res);
-
-                  
-                    // Console.WriteLine();
+        for (int r = 0; r <= maxLength; r++) {
+            for (int c = 0; c <= maxLength; c++) {
+                if (!visited[r, c] && matrix[r, c] == 1) {
+                    ArratDFS(matrix, visited, r, c, r, c);
                 }
             }
         }
 
-        print(size, plane);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                Console.Write(plane[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        //  print(size, plane);
 
         return res;
     }
 
-    public void print(int size, int[,] plane) {
+    public string Find(int row, int col) {
+        string val = row + "#" + col;
+
+        while (val != plane[row, col]) {
+            val = plane[row, col];
+            (row, col) = ToPosition(val);
+        }
+
+        return val;
+    }
+
+    public void Union(int rowA, int colA, int rowB, int colB) {
+        string rootA = Find(rowA, colA);
+        string rootB = Find(rowB, colB);
+
+        if (rootA == rootB) {
+            return;
+        }
+
+        var(rootArow, rootAcol) = ToPosition(rootA);
+        var(rootBrow, rootBcol) = ToPosition(rootB);
+
+        // a smaller
+        if (size[rootArow, rootAcol] <= size[rootBrow, rootBcol]) {
+            plane[rootArow, rootAcol] = plane[rootBrow, rootBcol];
+            size[rootBrow, rootBcol] += size[rootArow, rootAcol];
+        } else {
+            // b smaller
+            plane[rootBrow, rootBcol] = plane[rootArow, rootAcol];
+            size[rootArow, rootAcol] += size[rootBrow, rootBcol];
+        }
+    }
+
+    public(int, int) ToPosition(string pos) {
+        string[] strArr = pos.Split("#");
+
+        return (int.Parse(pos[0].ToString()), int.Parse(pos[1].ToString()));
+    }
+
+    public void ArratDFS(int[, ] matrix, bool[, ] visited, int row, int col, int parentRow, int parentCol) {
+        if (row == matrix.GetLength(0) || col == matrix.GetLength(1) || visited[row, col]) {
+            return;
+        }
+
+        if (matrix[row, col] == 1) {
+            visited[row, col] = true;
+
+            // connect with parent;
+            Union(parentRow, parentCol, row, col);
+
+            for (int rr = 0; rr < matrix.GetLength(0); rr++) {
+                ArratDFS(matrix, visited, rr, col, row, col);
+            }
+
+            for (int cc = 0; cc < matrix.GetLength(1); cc++) {
+                ArratDFS(matrix, visited, row, cc, row, col);
+            }
+        }
+    }
+
+    public void print(int size, int[, ] plane) {
         for (int i = 0; i <= size; i++) {
             for (int j = 0; j <= size; j++) {
                 Console.Write($"{plane[i, j]} ");
@@ -56,7 +123,7 @@ public class Solution {
         }
     }
 
-    public void CountRemoveStones(int[,] plane, bool[,] visited, int r, int c, ref int res) {
+    public void CountRemoveStones(int[, ] plane, bool[, ] visited, int r, int c, ref int res) {
         if (r == plane.GetLength(0) || c == plane.GetLength(1)) {
             return;
         }
@@ -131,4 +198,3 @@ public class Solution {
     }
 }
 // @lc code=end
-
