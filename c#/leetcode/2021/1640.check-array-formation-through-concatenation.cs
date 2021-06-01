1264a0581bf134452996ad3c850b79711aac2b1a
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-01 13:37:39 +0600

    1640. Check Array Formation Through Concatenation

---
+++ c#/leetcode/2021/1640.check-array-formation-through-concatenation.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1640 lang=csharp
+ *
+ * [1640] Check Array Formation Through Concatenation
+ */
+
+// @lc code=start
+public class Solution {
+    public bool CanFormArray(int[] arr, int[][] pieces) {
+        
+    }
+}
+// @lc code=end
+
