b98821b05badc0abd3ea672c05b3bd92d3cb340b
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-02 13:38:37 +0600

    [1640] Check Array Formation Through Concatenation

--- c#/leetcode/2021/1640.check-array-formation-through-concatenation.cs
+++ c#/leetcode/2021/1640.check-array-formation-through-concatenation.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 /*
  * @lc app=leetcode id=1640 lang=csharp
  *
@@ -7,8 +12,31 @@
 // @lc code=start
 public class Solution {
     public bool CanFormArray(int[] arr, int[][] pieces) {
-        
+
+        Dictionary<int, int[]> dic = new Dictionary<int, int[]>();
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            dic.Add(pieces[i][0], pieces[i]);
+        }
+
+        for (int i = 0; i < arr.Length;)
+        {
+            if(!dic.ContainsKey(arr[i])) return false;
+
+            var pieceArr = dic[arr[i]];
+
+            for (int j = 0; j < pieceArr.Length; j++)
+            {
+                if(arr[i++] != pieceArr[j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
+
 }
 // @lc code=end
 
