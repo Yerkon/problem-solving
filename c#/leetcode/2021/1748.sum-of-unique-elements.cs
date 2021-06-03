c963293da801cabe7f1cdaa16d95f0cc29944e30
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-03 09:34:10 +0600

    [1748] Sum of Unique Elements

--- c#/leetcode/2021/1748.sum-of-unique-elements.cs
+++ c#/leetcode/2021/1748.sum-of-unique-elements.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 /*
  * @lc app=leetcode id=1748 lang=csharp
  *
@@ -11,7 +12,50 @@ using System.Text;
 
 // @lc code=start
 public class Solution {
-    public int SumOfUnique(int[] nums) {
+
+    // two hashsets
+     public int SumOfUnique(int[] nums) {
+        int res = 0;
+        
+        var set1 = new HashSet<int>();
+        var set2 = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if(set1.Add(num)) {
+                set2.Add(num);
+            } else if(set2.Contains(num)) {
+                set2.Remove(num);
+            }
+        }
+
+        return set2.Sum();
+    }
+
+
+ // hashmap two loops
+    public int SumOfUnique2(int[] nums) {
+        int res = 0;
+        
+        var map = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            map[num] = map.GetValueOrDefault(num, 0) + 1;
+        }
+
+        foreach (var num in nums)
+        {
+            if(map[num] == 1) {
+                res += num;
+            }
+        }
+
+        return res;
+    }
+
+    // array two loops
+    public int SumOfUnique1(int[] nums) {
         int res = 0;
         
         int[] countArr = new int[101];
