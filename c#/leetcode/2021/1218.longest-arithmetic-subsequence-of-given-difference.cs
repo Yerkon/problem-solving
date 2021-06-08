f6c9250d6cffa33f3aa22e371d9d247ad0385b74
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-08 13:16:36 +0600

    [1218] Longest Arithmetic Subsequence of Given Difference: init

---
+++ c#/leetcode/2021/1218.longest-arithmetic-subsequence-of-given-difference.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1218 lang=csharp
+ *
+ * [1218] Longest Arithmetic Subsequence of Given Difference
+ */
+
+// @lc code=start
+public class Solution {
+    public int LongestSubsequence(int[] arr, int difference) {
+        
+    }
+}
+// @lc code=end
+
