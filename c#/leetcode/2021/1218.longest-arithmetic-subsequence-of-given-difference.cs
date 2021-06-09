ade508e932ac81bb813c1fab41cb3e5ae99db080
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-09 09:38:19 +0600

    [1218] Longest Arithmetic Subsequence of Given Difference

--- c#/leetcode/2021/1218.longest-arithmetic-subsequence-of-given-difference.cs
+++ c#/leetcode/2021/1218.longest-arithmetic-subsequence-of-given-difference.cs
@@ -12,20 +12,40 @@ using System.Text;
 // @lc code=start
 public class Solution {
 
-    // [1,2,3,4]
     public int LongestSubsequence(int[] arr, int difference) {
-        if(arr.Length == 1) return 1;
+        if (arr.Length == 1) return 1;
+
+        int res = 1;
+        var map = new Dictionary<int, int>();
+      
+        for (int i = 0; i < arr.Length; i++) {
+            int key = arr[i] + difference;
+            
+            map[key] = map.GetValueOrDefault(key, 0);
+            map[arr[i]] = map.GetValueOrDefault(arr[i], 0);
+
+            map[key] = map[arr[i]] + 1;
+
+            res = Math.Max(res, map[key]);
+        }
+
+        return res;
+    }
+
+    // [1,2,3,4]
+    // brute force
+    public int LongestSubsequence1(int[] arr, int difference) {
+        if (arr.Length == 1) return 1;
         int res = 1;
 
-        for (int i = 0; i < arr.Length; i++)
-        {
+        for (int i = 0; i < arr.Length; i++) {
             int curr = arr[i] + difference;
             int count = 1;
             int j = i + 1;
 
-            while (j < arr.Length)
-            {
-                if(arr[j] == curr) {
+            while (j < arr.Length) {
+
+                if (arr[j] == curr) {
                     count++;
                     curr += difference;
                 }
