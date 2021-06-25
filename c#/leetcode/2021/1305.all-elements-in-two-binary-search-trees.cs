9639afa89e8ec2f39331f18d143f44ddbb30448f
Yerkebulan Abzhapbarov <abzhaparov@strongte.am>
2021-06-25 09:41:35 +0600

    start [1305] All Elements in Two Binary Search Trees

---
+++ c#/leetcode/2021/1305.all-elements-in-two-binary-search-trees.cs
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1305 lang=csharp
+ *
+ * [1305] All Elements in Two Binary Search Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> GetAllElements(TreeNode root1, TreeNode root2) {
+        
+    }
+}
+// @lc code=end
+
